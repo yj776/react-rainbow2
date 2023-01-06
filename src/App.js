4676f8588d49385b3647e4e3227b07233993f84e
Amii Jiang <amii@macbook-pro.mynetworksettings.com>
2023-01-06 16:41:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import ColorBlock from './ColorBlock'
 
 function App() {
+  let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+  let colorMap = colors.map((color,i) => {
+    return (
+      <ColorBlock color = {color} />
+    )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {colorMap}
     </div>
   );
 }
